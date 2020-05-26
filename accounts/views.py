d93e4e22a9cfa4f9fbca6ec46759fad09d4413c1
yashladd <yashlad.08@gmail.com>
2020-05-27 00:44:37 +0530

    Register functionality complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'TEst')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(
+                        request, 'You are now registered and can log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
