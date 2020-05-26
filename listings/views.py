8684e351165603a1b3c3d5044f2fcee2f7b3062f
yash <yash.lad@siemens.com>
2020-05-26 19:23:12 +0530

    Search by filters functionality implemented

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -16,8 +17,50 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query_set_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query_set_list = query_set_list.filter(
+                description__icontains=keywords)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query_set_list = query_set_list.filter(
+                city__iexact=city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query_set_list = query_set_list.filter(
+                state__iexact=state)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query_set_list = query_set_list.filter(
+                bedrooms__lte=bedrooms)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query_set_list = query_set_list.filter(
+                price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': query_set_list
+    }
+    return render(request, 'listings/search.html', context)
